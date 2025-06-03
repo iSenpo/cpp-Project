861cc0de59b3a1b2d71e792df9157df526e5ae12
iSenpo <196292286+iSenpo@users.noreply.github.com>
2025-06-03 19:36:12 +0330

    update

--- TODO-List/TODO-list.cpp
+++ TODO-List/TODO-list.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
 
+class User{
+    private:
+    string username;
+    string password;
+    public:
+    User(string a,string b): username(a), password(b){};
+};
+class TaskBase{};
+class TimedTask : public TaskBase{};
+class NormalTask : public TaskBase{
+    
+};
+
 int main(){
+
     return 0;
 }
