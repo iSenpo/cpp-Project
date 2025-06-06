ef23ec07ad2f8916578c7593af31906063cc33ff
iSenpo <196292286+iSenpo@users.noreply.github.com>
2025-06-06 17:30:22 +0330

    update

--- TODO-List/TODO-list.cpp
+++ TODO-List/TODO-list.cpp
@@ -3,98 +3,13 @@
 using namespace std;
 
 class User{
-    private:
+    protected:
     string username;
     string password;
     public:
-    User(string a,string b): username(a), password(b){
-        fstream file("UserInfo.txt", ios::in | ios::out | ios::app);
-        if(file.is_open()){
-            file<<"\n"<<username<<" "<<password;
-        }
-        else{
-            cerr<<"could not open file!";
-        }
-        file.close();
-    };
-};
-class TaskBase{
-    public:
-    virtual void ShowInfo() = 0;
-    virtual void AddItem() = 0;
-    virtual void DeleteItem() = 0;
-    virtual ~TaskBase(){}
-};
-class TodoList : public TaskBase{
-    private:
-    string t = "";
-    bool checked;
-    public:
-    void ShowInfo() override{
-
-    }
-    void AddItem() override{
-
-    }
-    void DeleteItem() override{
-
-    }
-};
-class Reminder : public TaskBase{
-    private:
-    int CreationDate;
-    int ExpireDate;
-    public:
-    void ShowInfo() override{
-
-    }
-    void AddItem() override{
-
-    }
-    void DeleteItem() override{
-
-    }
-};
-class ShopList : public TaskBase{
-    private:
-    int price;
-    public:
-    void ShowInfo() override{
-
-    }
-    void AddItem() override{
-
-    }
-    void DeleteItem() override{
-
-    }
-};
-bool isName(string s){
-    if((s[0] >= '0' && s[0] <= '9') || (s[0] == '.')){
-        return false;
-    }
-    for(int i =0; i < s.size();i++){    
-        if(!((s[i] >= 'A' && s[i] <= 'Z') 
-        || (s[i] >= 'a' && s[i] <= 'z')
-        || (s[i] >= '0' && s[i] <= '9')
-        || (s[i] == '.'))){
-            return false;
-        }
-    }
-    return true;
-}
-bool isPassword(string s){
-     for(int i =0; i < s.size();i++){    
-        if(!((s[i] >= 'A' && s[i] <= 'Z') || (s[i] >= 'a' && s[i] <= 'z')
-        || (s[i] >= '0' && s[i] <= '9') || (s[i] == '.')
-        || (s[i] == '!') || (s[i] == '@') || (s[i] == '#')
-        || (s[i] == '&') || (s[i] == '*') )){
-            return false;
-        }
-    }
-    return true;
-}
-void CreateAccount(){
+    User(){}
+    User(bool Account){
+        if(!Account){
     string u;
     string p;
     string p2;
@@ -112,6 +27,11 @@ void CreateAccount(){
                 cout<<"~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n"
                 <<"Invalid USERNAME!your USERNAME must strat with a letter (A-Z or a-z)\n"
                 <<"and you can use Numbers(0-9) or (.) in the middle if you want\n"
+                <<"try again now :    ";
+        }
+        else if(!checkUserExist(u)){
+             cout<<"~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n"
+                <<"USERNAME already taken!\n"
                 <<"try again now :    ";
         }
         else{
@@ -147,52 +67,210 @@ void CreateAccount(){
                     <<"~ ~ ~ Account Succecfully Created ~ ~ ~\n";
                 loop = false;
             }
-            }  
-    new User(u,p);
-}
-void Login(){
-    string u,p;
-    cout<<"Enter your USERNAME :    ";
-    cin>>u;
-}
-bool CheckAccountInfo(){
-    fstream file("UserInfo.txt", ios::in);
-    string t = "";
-    if(!file.is_open()){
-        cerr<<"Something Went wrong!";
+            }
+            CreateAccount(u,p); 
+        }
+        else{
+        bool loop = true;
+        while(loop){
+            string u,p;
+            cout<<"Enter your USERNAME :    ";
+            cin>>u;
+            cout<<"Enter your PASSWORD :    ";
+            cin>>p;
+            if(!CheckAccountInfo(u,p)){
+                cout<<"Wrong USERNAME or PASSWORD! try again:\n";
+            }
+            else{
+                cout<<"~ ~ ~ Succecfully Loged in ~ ~ ~\n";
+                username = u;
+                password = p;
+                loop = false;
+            }
+        }
+        }
     }
-    else{
-        getline(file,t);
+    bool checkUserExist(string t){
+        string s1,s2;
+        ifstream file("Accounts.txt");
+        if(!file.is_open()){
+        cerr<<"Something Went wrong!";
+        }
+        else{
+            while(file >> s1 >> s2){
+                if(s1 == t){
+                    return false;
+                }
+            }
+        }
+        file.close();
+    return true;
+    }
+    void CreateAccount(string a,string b){
+        username = a;
+        password = b;
+        fstream file("Accounts.txt",ios::app);
+        if(file.is_open()){
+            file<<username<<" "<<password<<"\n";
+        }
+        else{
+            cerr<<"could not open file!";
+        }
+        file.close();
+    } 
+    bool CheckAccountInfo(string s,string t){
+        ifstream file("Accounts.txt");
+        string fileUser,filePass;
+        if(!file.is_open()){
+            cerr<<"Something Went wrong!";
+        }
+        else{
+            while(file >> fileUser >> filePass){
+                if(fileUser == s && filePass == t){
+                    return true;
+                }
+            }
+        }
+    file.close();
+    return false;
+}  
+bool isName(string s){
+    if((s[0] >= '0' && s[0] <= '9') || (s[0] == '.')){
+        return false;
+    }
+    for(int i =0; i < s.size();i++){    
+        if(!((s[i] >= 'A' && s[i] <= 'Z') 
+        || (s[i] >= 'a' && s[i] <= 'z')
+        || (s[i] >= '0' && s[i] <= '9')
+        || (s[i] == '.'))){
+            return false;
+        }
     }
     return true;
 }
-void MainMenu(){
-     cout<<"~~~~~~~~~~~~~ Main Menu ~~~~~~~~~~~~~~~\n"
-        <<"1.Go to my TODO-List Menu\n"
-        <<"2.Go to my Reminder Menu\n"
-        <<"3.Go to my Shoping List Menu\n"
-        <<"0.quit program\n"
-        <<"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n";
+bool isPassword(string s){
+     for(int i =0; i < s.size();i++){    
+        if(!((s[i] >= 'A' && s[i] <= 'Z') || (s[i] >= 'a' && s[i] <= 'z')
+        || (s[i] >= '0' && s[i] <= '9') || (s[i] == '.')
+        || (s[i] == '!') || (s[i] == '@') || (s[i] == '#')
+        || (s[i] == '&') || (s[i] == '*') )){
+            return false;
+        }
+    }
+    return true;
 }
+    ~User(){
+        delete this;
+    }
+};
+  
+class TaskBase{
+    public:
+    virtual void ShowInfo() = 0;
+    virtual void AddItem() = 0;
+    virtual void DeleteItem() = 0;
+    virtual ~TaskBase(){}
+};
+class TodoList : public TaskBase,public User{
+    private:
+    string t;
+    string name;
+    public:
+    TodoList() : User(){}
+    void ShowInfo() override{
+        int ul = 1;
+        ifstream file ( name + "_TodoList.txt",ios::in);
+        if(!file.is_open()){
+            cerr<<"Semething went wrong!";
+        }
+        else{
+            cout<<"~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~\n";
+            while(getline(file,t)){
+                cout<<ul<<"."<<t<<"\n";
+                ul++;  
+            }
+            cout<<"~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~\n";
+        }
+        file.close();
+    }
+    void AddItem() override{
+        fstream file( username + "_TodoList.txt",ios::app);
+        if(!file.is_open()){
+            cerr<<"Something went wrong!";
+        }
+        else{
+            cout<<"Enter your task : \n";
+            cin.ignore();
+            getline(cin,t);
+            file << t << "\n";
+            cout<< "Task Succecfully added! \n";
+        }
+        file.close();
+    }
+    void DeleteItem() override{
+
+    }
+    ~TodoList(){
+        delete this;
+    }
+    friend class User;
+};
+class Reminder : public TaskBase{
+    private:
+    string name;
+    int CreationDate;
+    int ExpireDate;
+    public:
+    void ShowInfo() override{
+
+    }
+    void AddItem() override{
+
+    }
+    void DeleteItem() override{
+
+    }
+};
+class ShopList : public TaskBase{
+    private:
+    int price;
+    string name;
+    public:
+    void ShowInfo() override{
+
+    }
+    void AddItem() override{
+
+    }
+    void DeleteItem() override{
+
+    }
+};
+
+
 int main(){
+    TaskBase* Task[3];
+    Task[0] = new TodoList;
+    Task[1] = new Reminder;
+    Task[2] = new ShopList;
     cout<<"~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n"
         <<"1.Create New Account\n"
         <<"2.Log into Existing Account\n"
         <<"0.quit program\n"
         <<"~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n";
-        bool Stopit = true;
-    while(Stopit){
+        bool loop = true;
+    while(loop){
         int choice;
         cin>>choice;
         switch (choice){
             case (1):{
-               CreateAccount();
-               Stopit = false;
-               break;
+                User* uTemp = new User(0);
+                loop = false;
+                break;
             }
             case (2):{
-                Login();
-                Stopit = false;
+                User* uTemp = new User(1);
+                loop = false;
                 break;
             }
             case (0):{
@@ -206,6 +284,70 @@ int main(){
 
         }
     }
-    
+    cout<<"~~~~~~~~~~~~~ Main Menu ~~~~~~~~~~~~~\n"
+        <<"1.Go to my TODO-List Menu\n"
+        <<"2.Go to my Reminder Menu\n"
+        <<"3.Go to my Shoping List Menu\n"
+        <<"4.Log Out\n"
+        <<"0.quit program\n"
+        <<"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n";
+    loop = true;
+    while(loop){
+        int choice;
+        cin>>choice;
+        switch(choice){
+            case (1):{
+                bool loop1 = true;
+                while(loop1){
+                    cout<<"~ ~ ~ ~ ~ ~ TODO-LIST ~ ~ ~ ~ ~ ~\n"
+                        <<"1.Show My TODO-LIST\n"
+                        <<"2.Add a new Task to my List\n"
+                        <<"3.Delete an existing Task\n"
+                        <<"4.Back\n"
+                        <<"0.quit program\n"
+                        <<"~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~\n";
+                    int choice1;
+                    cin>>choice1;
+                    switch(choice1){
+                        case (1):{
+                            Task[0]->ShowInfo();
+                            break;
+                        }
+                        case (2):{
+                           Task[0]->AddItem();
+                           break;
+                        }
+                        case (3):{
+
+                        }
+                        case (4):{
+                            loop1 = false;
+                            break;
+                        }
+                        case (0):{
+                            cout<<"\nClosing Program... bye bye!";
+                            return 0;
+                        }
+                    }
+                }
+            }
+            case (2):{
+
+            }
+            case (3):{
+
+            }
+            case (4):{
+                main();
+            }
+            case (0):{
+                cout<<"\nClosing Program... bye bye!";
+                return 0;
+            }
+            default:{
+
+            }
+        }
+    }
     return 0;
 }
